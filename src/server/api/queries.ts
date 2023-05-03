c212f238dd281c1c7d1deba4709134bc253e543e
Victor Ionescu <dsign.victor@gmail.com>
2023-05-03 20:44:35 +0300

    first

---
+++ src/server/api/queries.ts
@@ -0,0 +1,23 @@
+import { query$ } from "@prpc/solid";
+import { z } from "zod";
+import { prisma } from "~/server/db";
+
+export const helloQuery = query$({
+  queryFn: ({ payload }) => {
+    return `server says hello: ${payload.name}`;
+  },
+  key: "hello",
+  schema: z.object({ name: z.string() }),
+});
+
+export const noteList = query$(
+  async ({ payload }) => {
+    return await prisma.notes.findMany({
+      where: {
+        userId: payload.id,
+      },
+    });
+  },
+  "note-list",
+  z.object({ id: z.string().min(3) })
+);
